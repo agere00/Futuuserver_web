cd7dc0c9b14e3fbcfd4801b8d9454bedb1ea84e1
agere00 <agere000@gmail.com>
2015-06-01 17:33:47 +0900

    first commit

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+﻿<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script>
+$(function () {
+  var $body = $('body');
+  $('#js__sideMenuBtn').on('click', function () {
+    $body.toggleClass('side-open');
+    $('#js__overlay').on('click', function () {
+      $body.removeClass('side-open');
+    });
+  });
+});
+</script>
